6fe3dfc6848e51c235ad73b372d91a19e975fadb
kwanwoo kim <kwanwoo.kim@gmail.com>
2022-12-07 22:23:21 +0900

    장고admin을 통한 데이터 관리 - admin에 Post 등록

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+# admin.site.register(Post) # 첫번째 방법
+
+#Post를 admin에 등록하는 세번째 방법
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass

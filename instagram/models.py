0d18c5cb037b0633b255bb0f88452b105c4b66bd
kwanwoo kim <kwanwoo.kim@gmail.com>
2022-12-07 22:23:50 +0900

    장고admin을 통한 데이터 관리 - str 출력 변경

--- instagram/models.py
+++ instagram/models.py
@@ -4,3 +4,8 @@ class Post(models.Model):
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        ##return f"Custom Post object({self.id})"
+        return self.message
+

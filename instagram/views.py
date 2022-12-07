554975e55c92cafd1040516541e9b7a2c821478c
kwanwoo kim <kwanwoo.kim@gmail.com>
2022-12-08 04:21:53 +0900

    Queryset 검색 구현 - post_list 구현

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    qs = Post.objects.all()
+    q = request.GET.get('q','') # 키가 없을때는 ''
+    if q:
+        qs = qs.filter(message__icontains=q)
+    # instagram/templates/instagram/post_list.html
+    return render(request, 'instagram/post_list.html', {
+        'post_list': qs,
+    })

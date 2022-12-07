7ff64f63c4e0e66349a890f659938e802e78e05c
kwanwoo kim <kwanwoo.kim@gmail.com>
2022-12-08 03:41:47 +0900

    장고 media 파일 - Photo tag style tag

--- instagram/admin.py
+++ instagram/admin.py
@@ -15,7 +15,7 @@ class PostAdmin(admin.ModelAdmin):
 
     def photo_tag(self, post):
         if post.photo:
-            return mark_safe(f'<img src ="{post.photo.url}" />')
+            return mark_safe(f'<img src ="{post.photo.url}" style="width: 72px;" />')
         return None
 
     # def message_length(self, post):

f6c23c4c00a458e3d312fe1800b8515652303c2a
kwanwoo kim <kwanwoo.kim@gmail.com>
2022-12-08 03:12:47 +0900

    장고admin을 통한 데이터 관리 - search 옵션 제공, list_filter

--- instagram/admin.py
+++ instagram/admin.py
@@ -6,8 +6,10 @@ from .models import Post
 #Post를 admin에 등록하는 세번째 방법
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['pk', 'message', 'message_length', 'created_at', 'updated_at'] # admin 화면의 보여줄 화면 설정
+    list_display = ['pk', 'message', 'message_length', 'is_public', 'created_at', 'updated_at'] # admin 화면의 보여줄 화면 설정
     list_display_links = ['message']
+    list_filter = ['created_at', 'is_public']
+    search_fields = ['message'] # search 옵션 제공
 
     # def message_length(self, post):
     #     return f"{len(post.message)} 글자"

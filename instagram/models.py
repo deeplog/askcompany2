583ea0904cb04291402ffeea9e9bd808579aac8b
kwanwoo kim <kwanwoo.kim@gmail.com>
2022-12-24 16:15:12 +0900

    관계를 표현하는 모델 필드 (ForeignKey) - author 키 생성 및 migration

--- instagram/models.py
+++ instagram/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.conf import settings
 
 class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     message = models.TextField()
     photo = models.ImageField(blank=True, upload_to='instagram/post/%Y%m%d')
     is_public = models.BooleanField(default=False, verbose_name='공개여부')

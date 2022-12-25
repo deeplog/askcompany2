92eb66492145d8430d969f0b3c170cde3fa11167
kwanwoo kim <kwanwoo.kim@gmail.com>
2022-12-26 04:25:01 +0900

    관계를 표현하는 모델 필드 (ManyToManyField) - admin 등록

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 
-from .models import Post, Comment
+from .models import Post, Comment, Tag
 
 # admin.site.register(Post) # 첫번째 방법
 
@@ -23,4 +23,9 @@ class PostAdmin(admin.ModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass

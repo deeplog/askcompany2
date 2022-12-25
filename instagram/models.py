a4402adf3725c7d83be2a6dadc022b78810a0746
kwanwoo kim <kwanwoo.kim@gmail.com>
2022-12-26 04:17:28 +0900

    관계를 표현하는 모델 필드 (ManyToManyField) - Tag 생성

--- instagram/models.py
+++ instagram/models.py
@@ -5,6 +5,7 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     message = models.TextField()
     photo = models.ImageField(blank=True, upload_to='instagram/post/%Y%m%d')
+    tag_set = models.ManyToManyField('Tag', blank=True) # manytomany에서는 True로 지정
     is_public = models.BooleanField(default=False, verbose_name='공개여부')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -24,5 +25,13 @@ class Comment(models.Model):
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
 
 

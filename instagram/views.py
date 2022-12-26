396fbd1cb0a8ab9a295aff57de1537af525ab029
kwanwoo kim <kwanwoo.kim@gmail.com>
2022-12-27 05:24:17 +0900

    다양한 응답의 함수 기반 뷰 - CBV에 의한 List View 구현 (검색 이외의 기능은 동작)

--- instagram/views.py
+++ instagram/views.py
@@ -1,17 +1,20 @@
+from django.views.generic import ListView
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from .models import Post
 
-def post_list(request):
-    qs = Post.objects.all()
-    q = request.GET.get('q','') # 키가 없을때는 ''
-    if q:
-        qs = qs.filter(message__icontains=q)
-    # instagram/templates/instagram/post_list.html
-    return render(request, 'instagram/post_list.html', {
-        'post_list': qs,
-        'q':q, #input type에 전달
-    })
+post_list = ListView.as_view(model=Post)
+
+# def post_list(request):
+#     qs = Post.objects.all()
+#     q = request.GET.get('q','') # 키가 없을때는 ''
+#     if q:
+#         qs = qs.filter(message__icontains=q)
+#     # instagram/templates/instagram/post_list.html
+#     return render(request, 'instagram/post_list.html', {
+#         'post_list': qs,
+#         'q':q, #input type에 전달
+#     })
 
 
 def post_detail(request: HttpRequest, pk) -> HttpResponse:

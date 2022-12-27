7281516db25f03e95fff59c30dc60c74b4aadfdd
kwanwoo kim <kwanwoo.kim@gmail.com>
2022-12-28 05:24:06 +0900

    URL Dispatcher와 정규 표현식 - 년도로 매칭하게 수정

--- instagram/urls.py
+++ instagram/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns =[
     path('', views.post_list),
     path('<int:pk>/', views.post_detail),
-    path('archives/<int:year>/', views.archives_year),
+    # path('archives/<int:year>/', views.archives_year),
+    re_path('archives/(?P<year>\d{4})/', views.archives_year)
     # re_path(r'(?P<pk>\d+)/$', views.post_detail())
 ]

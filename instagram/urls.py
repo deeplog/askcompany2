35bc7699f5ab38c6a6ad0fceea0d16efc5388b97
kwanwoo kim <kwanwoo.kim@gmail.com>
2022-12-28 05:30:03 +0900

    URL Dispatcher와 정규 표현식 -  커스텀 Path converter

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, re_path
+from django.urls import path, re_path, register_converter
 
 from . import views
 
@@ -11,10 +11,14 @@ class YearConverter:
     def to_url(self, value):
         return str(value) #url 리버스할때 URL 문자열로 변환
 
+
+register_converter(YearConverter, 'year')
+
 urlpatterns =[
     path('', views.post_list),
     path('<int:pk>/', views.post_detail),
     # path('archives/<int:year>/', views.archives_year),
-    re_path('archives/(?P<year>\d{4})/', views.archives_year)
+    #re_path('archives/(?P<year>\d{4})/', views.archives_year)
+    path('archives/<year:year>/', views.archives_year),
     # re_path(r'(?P<pk>\d+)/$', views.post_detail())
 ]

318c90116d60e66723c87456c311ca9cc2465e5c
kwanwoo kim <kwanwoo.kim@gmail.com>
2023-01-07 07:10:46 +0900

    장고 CBV API (Generaic display views) - ListView 하단 페이징 UI 구현

--- askcompany2/settings.py
+++ askcompany2/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #third apps
+    'bootstrap4',
     'django_extensions', #extensions 추가
     'debug_toolbar',
     #locals apps

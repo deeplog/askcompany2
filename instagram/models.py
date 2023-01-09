3e2c4afa34dc952fb3305f4547ee64ec7794cb13
kwanwoo kim <kwanwoo.kim@gmail.com>
2023-01-09 20:38:09 +0900

    URL Reverse를 통해 URL 문자열 및 응답 생성하기 - get_absolute_url 사용하기

--- instagram/models.py
+++ instagram/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.conf import settings
+from django.urls import reverse
+
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -13,6 +15,9 @@ class Post(models.Model):
     def __str__(self):
         ##return f"Custom Post object({self.id})"
         return self.message
+
+    def get_absolute_url(self):
+        return reverse('instagram:post_detail', args=[self.pk])
 
     # admin 단에 구현할 수도 있음
     def message_length(self):
